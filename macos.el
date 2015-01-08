884734904985aef73da66aac5e382b8e63f78f3b
villadora <jky239@gmail.com>
2015-01-08 09:36:27 +0800

    add dash support for macos

--- macos.el
+++ macos.el
@@ -2,3 +2,19 @@
 
 ;;(define-key (keymap-parent local-function-key-map) [S-tab] nil)
 
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Dash Helper
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-to-list 'load-path "~/.emacs.d/")
+(autoload 'dash-at-point "dash-at-point"
+  "Search the word at point with Dash." t nil)
+(global-set-key "\C-cd" 'dash-at-point)
+(global-set-key "\C-ce" 'dash-at-point-with-docset)
+
+
+;; narrow down to docset prefix
+(add-to-list 'dash-at-point-mode-alist '(perl-mode . "perl"))
+(add-to-list 'dash-at-point-mode-alist '(js2-mode . "JavaScript"))
+
